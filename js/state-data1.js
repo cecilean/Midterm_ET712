ce0cb803eebf79c7dfc2b6a88f5f52ca7e5c6b60
cecilean <cecilialeandro@gmail.com>
2019-03-26 18:02:58 -0400

    first commit

---
+++ js/state-data1.js
@@ -0,0 +1,83 @@
+﻿var stateData = {
+	usa: {
+		name: 'Flowers',
+		scient: 'fl',
+		common: 'Flower',
+		height: '2-6',
+		significance: 'Peace'		
+		
+	},
+	
+	su: {
+		name: 'Sunflowers',
+		scient: 'Helianthus Annuus',
+		common: 'Sunflower',
+		height: '3 -6 mt',
+		significance: 'joy and spontaneity'		
+		
+	},
+	ro: {
+		name: 'Rose',
+		scient: 'Rose',
+		common: 'Rosel or Rose',
+		height: '2-5 mt',
+		significance: 'Passion - Happiness'		
+		
+	},
+	hd: {
+		name: 'Hydrangea',
+		scient: 'Hydrangea Macrophylla',
+		common: 'Hydrangela',
+		height: '1 - 1.5 mt',
+		significance: 'femininity - elegant demeanor'		
+		
+	},
+	tu: {
+		name: 'Tulips',
+		scient: 'Tulips spp',
+		common: 'Tulips',
+		height: '25 - 60 cms.',
+		significance: 'Love'		
+		
+	},
+	da: {
+		name: 'Dahlias',
+		scient: 'Dahlia',
+		common: 'Dahlias, Dahlias pompon',
+		height: '0.10 - 1 m',
+		significance: 'Passion and Impulse.'		
+		
+	},
+	lil: {
+		name: 'Lilies',
+		scient: 'Germanic Iris',
+		common: 'Lilies',
+		height: '0.6 - 0.9 m',
+		significance: 'Purity.'		
+		
+	},
+	car: {
+		name: 'Carnation',
+		scient: 'Dianthus Caryophyllus',
+		common: 'Carnation',
+		height: '0.45- 0.60 m',
+		significance: 'Pure and Passionate Love.'		
+		
+	},
+	daf: {
+		name: 'Daffodil',
+		scient: 'Narcissus',
+		common: 'Daffodil',
+		height: '0.40 m',
+		significance: 'Inner Beauty.'		
+		
+	},
+	ch: {
+		name: 'Cherry Blossom',
+		scient: 'Prunus serrulata',
+		common: 'Cherry Blossom',
+		height: '6-12 m',
+		significance: 'Fragility - Beauty of Life.'
+	}
+	
+};
